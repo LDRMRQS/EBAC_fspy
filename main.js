bc64cda131b0883323fe096eb83c1187fd7e5f4e
LDRMRQS <romulomarquesx@gmail.com>
2025-07-26 19:34:19 -0300

    commit exercício ajax

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    fetch('https://api.github.com/users/LDRMRQS')
+    .then(function(res) {
+        return res.json();
+    })
+    .then(function(json) {
+        $('#name').text(json.name);
+        $('#username').text(json.login);
+        $('#avatar').attr('src', json.avatar_url);
+        $('#repos').text(json.public_repos);
+        $('#followers').text(json.followers);
+        $('#following').text(json.following);
+        $('#link').attr('href', json.html_url);
+    })
+})
