e53c9be29d145e0f9ae0f76417496cc57d7be476
LDRMRQS <romulomarquesx@gmail.com>
2025-11-30 23:20:55 -0300

    commit inicial execício

---
+++ exercicio.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function multiplicar(num1, num2) {
+    return num1 * num2;
+}
+;
+function dizOla(nome) {
+    return `Olá ${nome}`;
+}
+;
+console.log(multiplicar(3, 8));
+console.log(dizOla('Maico'));
+//# sourceMappingURL=exercicio.js.map
---
+++ exercicio.ts
@@ -0,0 +1,10 @@
+function multiplicar(num1: number, num2: number): number {
+    return num1 * num2;
+};
+
+function dizOla(nome: string): string {
+    return `Olá ${nome}`;
+};
+
+console.log(multiplicar(3,8));
+console.log(dizOla('Maico'));
